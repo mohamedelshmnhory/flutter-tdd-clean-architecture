f817232b7f62d6f39b91a6c3c5b7341e4288fc4e
mohamednasserMg <95683227+mohamednasserMg@users.noreply.github.com>
2022-02-09 10:14:55 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/resocoder/clean_architecture_tdd_course/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.reso.clean_architecture_tdd_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+//  override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    GeneratedPluginRegistrant.registerWith(this)
+//  }
+}
